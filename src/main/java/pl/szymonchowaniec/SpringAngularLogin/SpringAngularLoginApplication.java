40f1271e78405b815e71026b8750ecde014b8302
Szymon Chowaniec <szymonchowaniec@gmail.com>
2018-01-11 21:55:01 +0100

    Initial commit

---
+++ src/main/java/pl/szymonchowaniec/SpringAngularLogin/SpringAngularLoginApplication.java
@@ -0,0 +1,12 @@
+package pl.szymonchowaniec.SpringAngularLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularLoginApplication.class, args);
+	}
+}
